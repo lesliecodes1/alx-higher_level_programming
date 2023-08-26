16eb5ac200b149b41110bd4e8555c5f5b68feba0
lesliecodes1 <lsltry404@gmail.com>
2023-08-26 17:03:33 +0300

    task 7

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+first = word[:3]
+sec = word[-2:]
+third = word[1:-1]
+print(f"First 3 letters: {first}")
+print(f"Last 2 letters: {sec}")
+print(f"Middle word: {third}")
